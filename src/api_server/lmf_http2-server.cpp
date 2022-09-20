2970d99f54b8eb183fe492affb974415547f8b25
Bhavishya Sharma <cs20mtech12006@iith.ac.in>
2022-09-20 10:45:40 +0530

    update api_server to rel 15

--- src/api_server/lmf_http2-server.cpp
+++ src/api_server/lmf_http2-server.cpp
@@ -43,7 +43,6 @@
 
 using namespace nghttp2::asio_http2;
 using namespace nghttp2::asio_http2::server;
-//using namespace oai::lmf_server::model;
 using namespace config;
 
 extern lmf_config lmf_cfg;
@@ -57,19 +56,15 @@ void lmf_http2_server::start() {
   // Default API
   /* TODO: Confirm base uri */
   server.handle(
-      NLMF_AUTH_BASE + lmf_cfg.sbi_api_version,
+      NLMF_AUTH_BASE + lmf_cfg.sbi_api_version + NLMF_DETERMINE_LOCATION,
       [&](const request& request, const response& response) {
         request.on_data([&](const uint8_t* data, std::size_t len) {
           std::string msg((char*) data, len);
           try {
             std::vector<std::string> split_result;
-            boost::split(
-                split_result, request.uri().path, boost::is_any_of("/"));
+            boost::split(split_result, request.uri().path, boost::is_any_of("/"));
             if (request.method().compare("POST") == 0 && len > 0) {
-              /*AuthenticationInfo authenticationInfo;
-              nlohmann::json::parse(msg.c_str()).get_to(authenticationInfo);
-              this->ue_authentications_post_handler(
-                  authenticationInfo, response);*/
+
             }
           } catch (std::exception& e) {
             Logger::lmf_server().warn(
@@ -85,4 +80,21 @@ void lmf_http2_server::start() {
   if (server.listen_and_serve(ec, m_address, std::to_string(m_port))) {
     std::cerr << "HTTP Server error: " << ec.message() << std::endl;
   }
+}
+
+void lmf_http2_server::detemine_location_post_handler(
+    const oai::lmf_server::model::InputData& inputData, const response& response) {
+  Logger::lmf_server().info("Received determine_location_post Request");
+
+  nlohmann::json locationData_json   = {};
+  Pistache::Http::Code code       = {};
+  header_map h;
+
+  m_lmf_app->handle_determine_location(inputData, locationData_json, code, 2);
+
+  if (code == Pistache::Http::Code::Ok)
+    response.write_head(HTTP_STATUS_CODE_200_OK, h);
+  else if (code == Pistache::Http::Code::Internal_Server_Error)
+    response.write_head(HTTP_STATUS_CODE_500_INTERNAL_SERVER_ERROR, h);
+  response.end(locationData_json.dump().c_str());
 }
