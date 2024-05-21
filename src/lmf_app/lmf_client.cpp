9c785c19a556c986880a43df4b7e0dae295fb7cc
Raphael Defosseux <raphael.defosseux@openairinterface.org>
2024-05-21 14:41:54 +0200

    chore(ci): proper format
    Signed-off-by: Raphael Defosseux <raphael.defosseux@openairinterface.org>

--- src/lmf_app/lmf_client.cpp
+++ src/lmf_app/lmf_client.cpp
@@ -89,7 +89,8 @@ void lmf_client::curl_http_client(
             std::string(CURL_MIME_BOUNDARY);
         headers = curl_slist_append(headers, content_type.c_str());
       } else if (method == "PATCH") {
-        headers = curl_slist_append(headers, "Content-Type: application/json-patch+json");
+        headers = curl_slist_append(
+            headers, "Content-Type: application/json-patch+json");
       } else {
         headers = curl_slist_append(headers, "Content-Type: application/json");
       }
