686645c77392eaf3556cca81d1d5d52a511a5e3b
Nima Chaharba <chaharba@sithonia.sboai.cs.eurecom.fr>
2026-06-23 17:44:01 +0200

    push

--- src/lmf_app/position_estimation.hpp
+++ src/lmf_app/position_estimation.hpp
@@ -1,22 +1,20 @@
 /*
  * SPDX-License-Identifier: LicenseRef-CSSL-1.0
  */
-
 #ifndef POSITION_ESTIMATION_HPP
 #define POSITION_ESTIMATION_HPP
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 int inverse_matrix(double A[2][2], double A_inv[2][2]);
 
+/* NOTE: pos_est is now double[3] (x, y, z). */
 void lls_estimation(
     double trp_pos[][3], int trp_pos_size, double dd_estimated[],
-    int dd_estimated_size, double pos_est[2]);
+    int dd_estimated_size, double pos_est[3]);
 
 #ifdef __cplusplus
 }
 #endif
-
 #endif  // POSITION_ESTIMATION_HPP
